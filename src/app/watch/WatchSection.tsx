20c6c88597c089589942a487393ed538bceb4590
HNamm <80162506+huydre@users.noreply.github.com>
2023-08-05 23:05:17 +0700

    Update Watch Section

--- src/app/watch/WatchSection.tsx
+++ src/app/watch/WatchSection.tsx
@@ -1,12 +1,17 @@
+"use client";
+import Recommendation from "@/components/shared/Recommendation";
 import React from "react";
 
 export interface WatchSectionProps {
-    id: string;
+  id: string;
+  data: any;
+  recommend: any;
 }
 
-const WatchSection = ({id}: WatchSectionProps) => {
+const WatchSection = ({ id, data, recommend }: WatchSectionProps) => {
+  console.log(data);
   return (
-    <div className="min-h-screen pt-[100px]">
+    <div className="min-h-screen pt-[70px]">
       <iframe
         src={`https://autoembed.to/movie/tmdb/${id}?server=3`}
         width="100%"
@@ -15,6 +20,17 @@ const WatchSection = ({id}: WatchSectionProps) => {
         allowFullScreen
         className="w-full aspect-video sm: pr-4 pl-4"
       />
+      <div className="space-y-4 pt-8 xl:px-32 px-4">
+        <h3>{data.title}</h3>
+        <p className="text-gray-200">
+          Phát hành vào ngày {data.release_date.slice(8, 10)} tháng{" "}
+          {data.release_date.slice(5, 7)} năm {data.release_date.slice(0, 4)}
+        </p>
+        <p className="line-clamp-3 text-gray-400">{data.overview}</p>
+        <Recommendation data={recommend.results}
+              title="Có thể bạn cũng thích"
+              mediatype={"movie"}/>
+      </div>
     </div>
   );
 };
--- src/app/watch/[...id]/page.tsx
+++ src/app/watch/[...id]/page.tsx
@@ -1,6 +1,8 @@
 import Layout from '@/components/shared/Layout'
 import React from 'react'
 import WatchSection from '../WatchSection'
+import getDetailMovie from '../../../../api/getDetailMovie'
+import getRecommendationMovie from '../../../../api/getRecommendationMovie';
 
 interface pageProps {
     params: {
@@ -8,12 +10,14 @@ interface pageProps {
     };
   }
 
-const Page = ({ params } : pageProps) => {
+const Page = async ({ params } : pageProps) => {
     const id = params.id[1];
+    const data = await getDetailMovie(id);
+    const recommendation = await getRecommendationMovie(id);
     console.log(id)
   return (
     <Layout>
-        <WatchSection id={id} />
+        <WatchSection id={id} data={data} recommend={recommendation}/>
     </Layout>
   )
 }
