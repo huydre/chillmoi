e96efb0178ad4befeb36e2bc9c0854224f63e4ed
HNamm <80162506+huydre@users.noreply.github.com>
2023-08-06 00:16:58 +0700

    Fix deploy error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import LastestUpdateSection from "@/components/Home/LastestUpdateSection";
 import PopularSection from "@/components/Home/PopularSection";
-import { Suspense } from "react";
-import Loading from "@/components/shared/loading";
 import Layout from "@/components/shared/Layout";
 import SwiperHeroSection from "@/components/Home/SwiperHeroSection";
 
