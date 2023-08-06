d55974bfc0e59fc76259c43de8778fb22106b7c4
HNamm <80162506+huydre@users.noreply.github.com>
2023-08-06 13:12:55 +0700

    Update Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,10 +5,10 @@ import { FaTiktok } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div className="w-full flex justify-center pt-24 pb-8 mx-10">
+    <div className="w-full flex justify-center pt-24 pb-8 lg:mx-10">
       <div className="text-center space-y-4">
         <h2>Chill<span className="text-primary">moi</span></h2>
-        <p className="text-gray-400 max-w-xl">Chillmoi không lưu trữ bất kì tệp tin nào trên máy chủ, chúng tôi chỉ liên kết tới những phương tiện truyền thông được lưu trữ bên dịch vụ thứ 3.</p>
+        <p className="text-gray-400 max-w-xl text-sm">Chillmoi không lưu trữ bất kì tệp tin nào trên máy chủ, chúng tôi chỉ liên kết tới những phương tiện truyền thông được lưu trữ bên dịch vụ thứ 3.</p>
         <div className="flex space-x-2 justify-center my-6">
           <AiFillFacebook />
           <BsDiscord />
