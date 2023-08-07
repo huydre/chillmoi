4aeab88881a2efb7ffab4380f96168ef4d7a423d
HNamm <80162506+huydre@users.noreply.github.com>
2023-08-07 14:16:35 +0700

    Add progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+"use client"
 import './globals.css'
 import { Montserrat } from 'next/font/google'
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -20,6 +22,12 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <ProgressBar
+          height="4px"
+          color="#5179FF"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
       </body>
     </html>
   )
